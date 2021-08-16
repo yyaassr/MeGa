6db797db56adcf172f6b29fa55f153ab4ebf3c13
yyaassr <88992970+yyaassr@users.noreply.github.com>
2021-08-16 04:15:06 +0300

    logs

---
+++ Config.lua
@@ -0,0 +1,31 @@
+--[[ کانال سورس خونه ! پر از سورس هاي ربات هاي تلگرامي !
+لطفا در کانال ما عضو شويد 
+@source_home
+https://t.me/source_home ]]
+SUDO_ID = {1454888224,1095129965,1694869617} --آیدی سودو های ربات (ربات cli , api  قرار بدید)
+Full_Sudo = {1454888224} -- آیدی سودو اصلی
+TD_ID = 1694869617 -- آیدی ربات
+Sudoid = 1454888224 -- آیدی سودو اصلی
+BotHelper = 1095129965 -- آیدی هلپر
+UserBotHelper = '@source_home' -- یوزرنیم هلپر
+LinkSuppoRt = 'https://t.me/joinchat/Cw7PwE_Q8KIUAw8u8692tw' -- لینک گروه ساپورت
+UserSudo = '@source_home' -- یوزرنیم سازنده
+PvUserSudo = '@source_home' -- پیام رسان سازنده
+Channel = '@source_home' -- یوزرنیم کانال 
+Channell = '@source_home'
+Channelll = '@source_home'
+ChannelInline = 'source_home' --یوزنیم کانال بدون @
+RedisIndex = 4
+token = '1095129965:AAEfRVVelrZDvwyjjCjDiXTJkb7Fi6YJttE' -- توکن ربات api
+--[[ کانال سورس خونه ! پر از سورس هاي ربات هاي تلگرامي !
+لطفا در کانال ما عضو شويد 
+@source_home
+https://t.me/source_home ]]
+--[[  آموزش نصب : 
+unzip MeGa+
+chmod +x MeGa+
+./MeGa+ install
+./MeGa+ config
+./MeGa+ login
+chmod +x Autolaunch
+screen ./Autolaunch ]]
